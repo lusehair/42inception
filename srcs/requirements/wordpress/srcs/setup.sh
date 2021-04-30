a67d34306882ff1b76a344c3a2e6b09f3a505b82
lusehair <lusehair@student.42.fr>
2021-04-30 17:28:29 +0200

    before the lockdone

--- srcs/requirements/wordpress/srcs/setup.sh
+++ srcs/requirements/wordpress/srcs/setup.sh
@@ -1,10 +1,10 @@
 while true; 
 do 
-    wp core install --url='https://'$EXT_IP':3306' \
+    wp core install --allow-root --url='https://'$EXT_IP':3306' \
     --title='WordPress for ft_services' --admin_user=''$WP_LOGIN'' --admin_password=''$WP_PASS'' \
     --admin_email="admin@admin.fr" && break; done ; 
 
-wp  user create $WPU_1LOGIN user1@user.com --user_pass=$WPU_1PASS
-wp  user create $WPU_2LOGIN user2@user.com --user_pass=$WPU_2PASS --role=author
+wp --allow-root user create $WPU_1LOGIN user1@user.com --user_pass=$WPU_1PASS
+wp --allow-root user create $WPU_2LOGIN user2@user.com --user_pass=$WPU_2PASS --role=author
 php-fpm7 
 nginx -g "deamon off;"

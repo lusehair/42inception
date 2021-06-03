9a1f1263f12837392be958bd18e7c2cff44f34bb
lusehair <lusehair@student.42.fr>
2021-06-03 16:38:03 +0200

    Everything seems ok, Makefile needed

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,6 +1,3 @@
-# service mysql start
-
-
 if [ ! -d "/var/lib/mysql/wordpress" ]; then 
     
     mysql_install_db
@@ -10,17 +7,7 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_LOGIN}'@'%' IDENTIFIED BY '${MARIA_PASS}';" &&\
 	mysql -e "FLUSH PRIVILEGES;"
-    #CMD to allow an external connection with le MARIA_LOGIN account 
-    #mysql -e "update mysql.user set host='%' where user = '${MARIA_LOGIN}';"
-    
-    #echo "CREATE USER '${MARIA_LOGIN}'@'localhost' identified by '${MARIA_PASS}';" | mysql -u root --skip-password
-    #echo "CREATE DATABASE IF NOT EXISTS wordpress;"  | mysql -u root --skip-password
-    #echo "GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIA_LOGIN'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-    #echo "GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIA_LOGIN'@'$WP_IP' IDENTIFIED BY '$MARIA_PASS' WITH GRANT OPTION;" | mysql -u root --skip-password
-    #echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-    #echo "exit;"
     service mysql stop 
 fi
-#wait 7
-
+sleep 5
 mysqld
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,27 +1,18 @@
-# systemctl restart nginx 
-#service nginx start 
 
-# systemctl restart php-fpm7
-#service php7.3-fpm restart
-
-#sleep infinity
-
-#chown -R www-data:www-data /var/wordpress/
-
-wp core --allow-root download 
-
-sleep 5;
-while  [ ! -f /var/www/wordpress/wp-config.php ]; do   
-
-    # while true; do 
-    wp core config --allow-root --dbname=wordpress --dbuser=$MARIA_LOGIN --dbpass=$MARIA_PASS --dbhost=mariadb:3306
+sleep 8;
+if  [ ! -f /var/www/wordpress/wp-config.php ]; then 
     
-     #if [ -f /var/wordpress/wp-config.php ]; break; 
+    wp core --allow-root download --locale=fr_FR --force 
+    sleep 5;
+    while  [ ! -f /var/www/wordpress/wp-config.php ]; do   
 
-done 
-    wp core install --allow-root --url='lusehair.42.fr' --title='WordPress for Inception' --admin_user={$WP_LOGIN} --admin_password={$WP_PASS}  --admin_email="admin@admin.fr" --path='/var/www/wordpress';
+        
+        wp core config --allow-root --dbname=wordpress --dbuser=$MARIA_LOGIN --dbpass=$MARIA_PASS --dbhost=mariadb:3306
+        
+
+    done 
+    wp core install --allow-root --url='lusehair.42.fr' --title='WordPress for Inception' --admin_user=$WP_LOGIN --admin_password=$WP_PASS  --admin_email="admin@admin.fr" --path='/var/www/wordpress';
     wp  user create --allow-root $WPU_1LOGIN user2@user.com --user_pass=$WPU_1PASS --role=author
-#fi 
-
+    wp theme install --allow-root dark-mode --activate     
+fi 
 php-fpm7.3 --nodaemonize
-#nginx -g "deamon off;" 
